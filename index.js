7fb37ca83dc141f08a9b2774b41e84250cecfc03
donell <donell.wambua@student.moringaschool.com>
2023-06-17 11:28:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName="bob"
+
+const leastFavoriteCustomer="Migan" ;
+
+function upperCaseCustomerName () {
+    console.log(customerName.toUppercase) () ;
+}
+function setBestCustomer() {
+    bestCustomer="not bob" ;
+
+}
+function overwriteBestCustomer() {
+    window.bestCustomer="maybe bob";
+
+}
+
+function changeleastFavoriteCustomer() {
+    leastFavoriteCustomer="Nipsey" ;
+}
